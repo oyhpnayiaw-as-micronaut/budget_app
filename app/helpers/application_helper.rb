106ede9173e3c4aadff86ca2a0ef561d0f2eedbb
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-29 23:23:58 +0630

    Setup themes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def title
+    base_title = 'dBudget'
+
+    # get the current controller name
+    controller_name = controller.controller_name
+
+    base_title = "#{controller_name} | #{base_title}" unless controller_name == 'home'
+
+    base_title
+  end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,4 +21,24 @@ module.exports = {
     require("@tailwindcss/container-queries"),
     require("daisyui"),
   ],
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    themes: [
+      {
+        mytheme: {
+          primary: "#3778c3",
+          secondary: "#5fb523",
+          accent: "#dd06cf",
+          neutral: "#232130",
+          "base-100": "#F7F7F7",
+          info: "#5BA8D7",
+          success: "#118856",
+          warning: "#AA6C09",
+          error: "#EB335E",
+        },
+      },
+    ],
+  },
 };
