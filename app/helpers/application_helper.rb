97340818803b576bff9a0ef8dad17fc4873d3308
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-30 12:54:35 +0630

    Update home route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   def title
-    base_title = 'dBudget'
+    base_title = 'Daisy'
 
     # get the current controller name
     controller_name = controller.controller_name
 
-    base_title = "#{controller_name} | #{base_title}" unless controller_name == 'home'
+    base_title = "#{controller_name.capitalize} | #{base_title}" unless controller_name == 'categories'
 
     base_title
+  end
+
+  def home?
+    root_path == request.path
   end
 end
