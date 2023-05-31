699e98dc4d2e6372dc87442e2417ad0c5448e10c
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-31 10:58:01 +0630

    Add unit tests for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:category) { Category.new }
+
+    it 'should validate presence of user' do
+      category.valid?
+      expect(category.errors[:user]).to include('must exist')
+    end
+  end
+
+  describe 'associations' do
+    it 'should belong to user' do
+      category = Category.reflect_on_association(:user)
+      expect(category.macro).to eq(:belongs_to)
+    end
+
+    it 'should have many transactions' do
+      category = Category.reflect_on_association(:app_transactions)
+      expect(category.macro).to eq(:has_many)
+    end
+  end
 end
