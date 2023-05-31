79f28fb8ca163f9653ad3f30287b3e19fd09a530
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-31 11:00:27 +0630

    Add unit tests for transaction

--- spec/models/app_transaction_spec.rb
+++ spec/models/app_transaction_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe AppTransaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:app_transaction) { AppTransaction.new }
+
+    it 'should validate presence of user' do
+      app_transaction.valid?
+      expect(app_transaction.errors[:user]).to include('must exist')
+    end
+  end
+
+  describe 'associations' do
+    it 'should belong to user' do
+      app_transaction = AppTransaction.reflect_on_association(:user)
+      expect(app_transaction.macro).to eq(:belongs_to)
+    end
+
+    it 'should have many transactions' do
+      app_transaction = AppTransaction.reflect_on_association(:categories)
+      expect(app_transaction.macro).to eq(:has_many)
+    end
+  end
 end
